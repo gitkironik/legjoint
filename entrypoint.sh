409e9fb73e992a9fa0c078255f5e1b5e480fa713
gitkironik <95974891+gitkironik@users.noreply.github.com>
2022-11-25 23:21:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="91fc524c-6cf9-11ed-a1eb-0242ac120002"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="91fc524c-6cf9-11ed-a1eb-0242ac120002"
 fi
 echo ${Vmess_UUID}
 
